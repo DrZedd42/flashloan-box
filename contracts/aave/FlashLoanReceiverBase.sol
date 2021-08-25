67e15bf28bc4ef236efba114bd9dba7041d9fc43
DrZedd42 <Gary.l.symons@gmail.com>
2021-08-24 22:16:00 -1000

    Add files via upload
    adjust for solidity version compatibility

--- contracts/aave/FlashLoanReceiverBase.sol
+++ contracts/aave/FlashLoanReceiverBase.sol
@@ -1,6 +1,6 @@
-pragma solidity ^0.6.6;
+pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelinopenzeppelin-contracts/contracts/utils/math/SafeMath.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/utils/math/SafeMath.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./IFlashLoanReceiver.sol";
